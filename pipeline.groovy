f4581154bd91eea7412e47efb6a6791e489d1ad6
LNPappas <LPappas@gmail.com>
2022-03-10 10:36:20 -0800

    sh to get git ref

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,11 +5,10 @@ def call(kwargs) {
             stage("initialize"){
                 steps {
                     script {
-                        def GIT_REF = 'main'
-                        // def GIT_REF = sh(
-                        //     script: "git rev-parse --abbrev-ref HEAD"
-                        //     returnStatus: true
-                        // )
+                        def GIT_REF = sh(
+                            script: "git rev-parse --abbrev-ref HEAD"
+                            returnStatus: true
+                        )
                         println GIT_REF
                     }
                 }
