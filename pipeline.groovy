1ea024a0e7625895ba64d21faabe85af7d678cea
LNPappas <LPappas@gmail.com>
2022-03-10 10:32:53 -0800

    no $ for git_ref

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,15 +1,17 @@
-pipeline {
-    agent any
-    stages {
-        stage("initialize"){
-            steps {
-                script {
-                    def GIT_REF = 'main'
-                    // def GIT_REF = sh(
-                    //     script: "git rev-parse --abbrev-ref HEAD"
-                    //     returnStatus: true
-                    // )
-                    println $GIT_REF
+def call(kwargs) {
+    pipeline {
+        agent any
+        stages {
+            stage("initialize"){
+                steps {
+                    script {
+                        def GIT_REF = 'main'
+                        // def GIT_REF = sh(
+                        //     script: "git rev-parse --abbrev-ref HEAD"
+                        //     returnStatus: true
+                        // )
+                        println GIT_REF
+                    }
                 }
             }
         }
